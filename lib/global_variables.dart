b7ddcced891fa43cab641373892f7764ef1ed57d
DewanggaHaryono <86884251+DewanggaHaryono@users.noreply.github.com>
2024-06-13 22:32:06 +0700

    fixed home screen ui/ux

--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -6,6 +6,8 @@ const Color darkGrey = Color(0xFF232323);
 const Color white = Color(0xFFD9D9D9);
 const Color darkBlue = Color(0xFF08344c);
 const Color cream = Color(0xFFE7E2D2);
+const Color darklightblue = Color(0xFF002C3D);
+const Color grey = Color(0xFF36454F);
 
 const String logoFont = 'Metafiz Demo';
 const String bodyFont = 'SF Pro Display';
